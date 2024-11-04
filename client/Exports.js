07a311182b7c3d57318d0c659a1cab54c18948b6
[anudeep avula] <[anudeepavula009@gmail.com]>
2024-11-04 13:22:49 +0530

    minor errors

--- client/Exports.js
+++ client/Exports.js
@@ -6,9 +6,9 @@ import Profile from "./src/components/Profile.jsx";
 import Bookmarks from "./src/components/Bookmarks.jsx";
 import Categories from "./src/components/Categories.jsx";
 import TrendingTags from "./src/components/TrendingTags.jsx";
-import Login from "./src/auth/login.jsx";
+import Login from "./src/auth/Login.jsx";
 import Signup from "./src/auth/Signup.jsx";
-import BlogEditor from './src/texteditor/BlogEditor.jsx'
+import BlogEditor from "./src/texteditor/BlogEditor.jsx";
 
 export {
   Navbar,
